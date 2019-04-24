aab23d5e7cc7a6abf311407e894578be320db4fd
Chris Hart <chart@concord.org>
2019-04-24 18:34:17 -0400

    Initial work to make a Typescript definition from the JSON from Dataflow 2

---
+++ src/components/app.tsx
@@ -0,0 +1,51 @@
+import { inject, observer } from "mobx-react";
+import * as React from "react";
+import { BaseComponent, IBaseProps } from "./base";
+import { Text } from "./text";
+import {
+  DataflowDiagram,
+  DataflowBlock,
+  SensorBlockType,
+  IOType,
+  LogicBlockType
+} from "../models/dataflow/dataflow-diagram";
+import "./app.sass";
+
+interface IProps extends IBaseProps {}
+interface IState {}
+
+@inject("stores")
+@observer
+export class AppComponent extends BaseComponent<IProps, IState> {
+
+  public render() {
+    const { ui } = this.stores;
+    const a = new DataflowBlock(1, "a",
+      SensorBlockType.Oxygen, "things", [1], 1, 1, IOType.number, IOType.number, { x: 1, y: 1 }, undefined, 10);
+    const b = new DataflowBlock(2, "b",
+      SensorBlockType.Oxygen, "things2", [1], 1, 1, IOType.number, IOType.number, { x: 2, y: 2 }, undefined, 15);
+    const c = new DataflowBlock(3, "c",
+      LogicBlockType.Operator, "logic",
+      [0, 1], 2, 1, IOType.number, IOType.number, { x: 3, y: 3 }, undefined, "+");
+    const d = new DataflowBlock(4, "d",
+      LogicBlockType.Operator, "logic",
+      [0, 1], 2, 1, IOType.number, IOType.number, { x: 4, y: 4 }, undefined, "*");
+    const e = new DataflowBlock(5, "e",
+      LogicBlockType.Comparator, "logic",
+      [0, 1], 2, 1, IOType.number, IOType.number, { x: 5, y: 5 }, undefined, ">");
+    const f = new DataflowBlock(6, "f",
+      LogicBlockType.LogicOperator, "logic",
+      [0, 1], 2, 1, IOType.number, IOType.number, { x: 6, y: 6 }, undefined, "AND");
+
+    const diagram = new DataflowDiagram("test", "test", [a, b, c, d, e, f], 1);
+    return (
+      <div className="app">
+        <div><span className="infoLabel">Blocks: </span>A = {a.value}, B = {b.value}</div>
+        <div><span className="infoLabel">Sum A + B: </span>{c.currentValue(diagram)}</div>
+        <div><span className="infoLabel">Multiply A * B: </span>{d.currentValue(diagram)}</div>
+        <div><span className="infoLabel">Compare A > B: </span>{e.currentValue(diagram)}</div>
+        <div><span className="infoLabel">Logic A AND B: </span>{f.currentValue(diagram)}</div>
+      </div>
+    );
+  }
+}
---
+++ src/models/dataflow/dataflow-diagram.tsx
@@ -0,0 +1,192 @@
+// following style guide https://basarat.gitbooks.io/typescript/docs/styleguide/styleguide.html
+
+interface Diagram {
+  name: string;
+  displayedName: string;
+  blocks: Block[];
+  fileVersion: number;
+  archived: boolean;
+}
+
+interface Block {
+  id: number;
+  name: string;
+  blockType: SensorBlockType | OutputBlockType | LogicBlockType;
+  units: string;
+  sources: number[];
+  inputCount: number;
+  outputCount: number;
+  inputType: IOType;
+  outputType: IOType;
+  view: Point;
+  params?: Params;
+  value?: number | string;
+}
+
+export enum SensorBlockType { // rename to InputBlockType?
+  Temperature = 0,
+  Humidity,
+  CarbonDioxide,
+  Oxygen,
+  Light,
+  SoilMoisture,
+  ParticulateMatter
+}
+
+export enum OutputBlockType {
+  Relay = 7,
+  Plot,
+  DataStorage
+}
+
+// deviating from spec, Plus, Minus, Times, Divide become Operator
+// GT, LT, ET, NET become Comparator
+// And, Or, Not, Nand, Xor become LogicOperator
+export enum LogicBlockType {
+  Number = 10,
+  Timer,
+  Operator,
+  Comparator,
+  LogicOperator,
+  AbsValue,
+  MovingAverage,
+  ExpMovingAverage
+}
+export enum IOType { bool, number, image } // bool, number, image
+
+interface Params {
+  recordingInterval: number;
+  datasetLocation: string; // not sure what this is?
+  sequenceNames: string; // or is this an array?
+  secondsOn: number;
+  secondsOff: number;
+  period: number;
+}
+
+interface Point { // display data for positioning in diagram or on a plot
+  x: number;
+  y: number;
+}
+
+export class DataflowDiagram implements Diagram {
+  constructor(
+    public name: string,
+    public displayedName: string,
+    public blocks: Block[],
+    public fileVersion: number,
+    public archived: boolean = false
+  ) {
+  }
+}
+
+type DataCalc = (a: number, b: number) => number;
+
+export class DataflowBlock implements Block {
+  private calc: {[op: string]: DataCalc} = {
+    "+": (a: number, b: number): number => a + b,
+    "-": (a: number, b: number): number => a - b,
+    "*": (a: number, b: number): number => a * b,
+    "/": (a: number, b: number): number => a / b,
+    ">": (a: number, b: number): number => a > b ? 1 : 0,
+    "<": (a: number, b: number): number => a < b ? 1 : 0,
+    ">=": (a: number, b: number): number => a >= b ? 1 : 0,
+    "<=": (a: number, b: number): number => a <= b ? 1 : 0,
+    "AND": (a: number, b: number): number => a && b,
+    "OR": (a: number, b: number): number => a || b,
+    "NOT": (a: number, b: number): number => a !== b ? 1 : 0,
+    "NAND": (a: number, b: number): number => !(a && b) ? 1 : 0,
+    "XOR": (a: number, b: number): number =>  !(a || b) ? 1 : 0,
+  };
+
+  constructor(
+    public id: number,
+    public name: string,
+    public blockType: SensorBlockType | OutputBlockType | LogicBlockType,
+    public units: string,
+    public sources: number[],
+    public inputCount: number,
+    public outputCount: number,
+    public inputType: IOType,
+    public outputType: IOType,
+    public view: Point,
+    public params?: Params,
+    public value?: number | string
+  ) {
+  }
+
+  public isSensor() {
+    return SensorBlockType[this.blockType] !== undefined;
+  }
+  public isOutput() {
+    return OutputBlockType[this.blockType] !== undefined;
+  }
+  public isLogic() {
+    return LogicBlockType[this.blockType] !== undefined;
+  }
+
+  public currentValue(diagram: DataflowDiagram, output?: any) {
+    let val = this.value && typeof(this.value) === "number" ? this.value : 0;
+
+    // A sensor is an input block
+    if (this.isSensor()) {
+      if (this.inputCount > 0) {
+        // take inputs
+        val += this.inputCount;
+      }
+      return val;
+    }
+
+    // A logic block operates on inputs and produces a value
+    else if (this.isLogic()) {
+      if (this.inputCount > 1 && diagram) {
+        const values: number[] = [];
+        for (let i = 0; i < this.inputCount; i++) {
+          const inputValue = diagram.blocks[this.sources[i]].value;
+          // since a value could be a number, string or null, verify we have values
+          if (inputValue && typeof (inputValue) === "number") {
+            values.push(inputValue);
+          }
+        }
+        const operator = this.value ? this.value : "+";
+        // TODO: allow calc to take more than the first two values
+        val = this.calc[operator](values[0], values[1]);
+        return val;
+      } else return 0;
+    }
+    // An output controls a relay, or records to data storage or plot
+    else if (this.isOutput()) {
+      switch (this.blockType) {
+        case OutputBlockType.Relay:
+          // a relay signal is sent based on the input value (evaluated via logic)
+          return val;
+        case OutputBlockType.Plot:
+          (output as DataflowPlot).addPoint({ x: Date.now(), y: val });
+          return;
+        case OutputBlockType.DataStorage:
+          // serialize data?
+          return val;
+      }
+    }
+
+    return val;
+  }
+
+}
+interface Plot {
+  series: number;
+  points: Point[];
+}
+
+export class DataflowPlot implements Plot{
+  constructor(
+    public series: number,
+    public points: Point[]) {
+    this.points = [];
+  }
+  public addPoint(nextPoint: Point) {
+    this.points.push(nextPoint);
+  }
+  public getPlot() {
+    return this.points;
+  }
+}
