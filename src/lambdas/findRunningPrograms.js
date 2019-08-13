4c75b1c70bddc29a00b824745f890d8718d8217d
Sam Fentress <sfentress@concord.org>
2019-08-13 10:14:02 -0400

    Stringify returned count to JSON

--- src/lambdas/findRunningPrograms.js
+++ src/lambdas/findRunningPrograms.js
@@ -12,37 +12,37 @@ exports.handler = async (event) => {
 
   if (!programs || programs.length === 0) {
     console.log("no active programs");
-    return result;
-  }
-  result.count = programs.length;
-  for (let i = 0; i < programs.length; i++){
-    console.log("running: " + JSON.stringify(programs[i].programId));
-    // console.log(programs[i]);
-    const nextRunTime = programs[i].nextRunTime ? programs[i].nextRunTime : 0;
-    const interval = programs[i].runInterval ? programs[i].runInterval : 1000;
-    console.log("time since last run: " + (Date.now() - nextRunTime));
-    if (Date.now() - nextRunTime > interval) {
-      // update timestamp
-      await updateProgramNextRunTimestamp(programs[i].endTime);
+  } else {
+    result.count = programs.length;
+    for (let i = 0; i < programs.length; i++){
+      console.log("running: " + JSON.stringify(programs[i].programId));
+      // console.log(programs[i]);
+      const nextRunTime = programs[i].nextRunTime ? programs[i].nextRunTime : 0;
+      const interval = programs[i].runInterval ? programs[i].runInterval : 1000;
+      console.log("time since last run: " + (Date.now() - nextRunTime));
+      if (Date.now() - nextRunTime > interval) {
+        // update timestamp
+        await updateProgramNextRunTimestamp(programs[i].endTime);
 
-      lambda.invoke({
-        FunctionName: 'arn:aws:lambda:us-east-1:816253370536:function:executeDataflowProgram',
-        Payload: JSON.stringify(programs[i]) // pass params
-        , LogType: 'Tail'
-        , InvocationType: 'Event'
-      }, function (error, data) {
-        // console.log("invoke complete: ", error, data);
-        if (error) {
-          console.log('error', error);
-        }
-        // if (data) {
-        //   console.log("success");
-        // }
-      });
+        lambda.invoke({
+          FunctionName: 'arn:aws:lambda:us-east-1:816253370536:function:executeDataflowProgram',
+          Payload: JSON.stringify(programs[i]) // pass params
+          , LogType: 'Tail'
+          , InvocationType: 'Event'
+        }, function (error, data) {
+          // console.log("invoke complete: ", error, data);
+          if (error) {
+            console.log('error', error);
+          }
+          // if (data) {
+          //   console.log("success");
+          // }
+        });
+      }
     }
   }
   console.log("done");
-  return result;
+  return JSON.stringify(result);
 };
 // Note that the endTime field in dataflow-programs was set as a key, so must be unique. This could potentially be a problem?
 // though the chances of a program being created with the same timestamp to the ms are slim
