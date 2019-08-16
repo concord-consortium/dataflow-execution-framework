1d9191174930f43dfeeb1cfd95957b6650456df3
Chris Hart <chart@concord.org>
2019-08-16 13:04:35 -0400

    Add an endTime parameter that can be used to filter the fetch to a range

--- src/lambdas/fetchProgramData.js
+++ src/lambdas/fetchProgramData.js
@@ -1,41 +1,36 @@
 const AWS = require('aws-sdk');
 const dynamoDocClient = new AWS.DynamoDB.DocumentClient();
-const lambda = new AWS.Lambda({
-  region: 'us-east-1' //change to your region
-});
 
 exports.handler = async (event, context, callback) => {
   const result = {
     count: 0
   };
   if (!event.programId) return null;
-  if (event.time) console.log(event.time);
 
-  const data = await getProgamData(event.programId, event.time);
+  const data = await getProgamData(event.programId, event.time, event.endTime);
 
   if (!data || data.length === 0) {
     console.log("no recorded data");
   } else {
     result.count = data.length;
-    console.log(data);
   }
-  console.log("done");
   callback(null, result);
 };
 // Note that the endTime field in dataflow-programs was set as a key, so must be unique. This could potentially be a problem?
 // though the chances of a program being created with the same timestamp to the ms are slim
-async function getProgamData(programId, eventTime) {
+async function getProgamData(programId, eventTime, eventEndTime) {
   return new Promise((resolve, reject) => {
     const params = {
       TableName: "dataflow-data",
-      KeyConditionExpression: "programId = :v_programId AND #t > :v_time",
+      KeyConditionExpression: "programId = :v_programId AND #t BETWEEN :v_time AND :v_endtime",
       ExpressionAttributeNames: {
         "#t": "time",
         "#v": "values"
       },
       ExpressionAttributeValues: {
         ":v_programId": programId,
-        ":v_time": eventTime ? eventTime : 0
+        ":v_time": eventTime ? eventTime : 0,
+        ":v_endtime": eventEndTime ? eventEndTime : Date.now()
       },
       ProjectionExpression: "programId, #t, blockIds, #v"
     };
